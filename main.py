db68d94164e6f90350d1fb2beb9585cd62f3aaa0
vikramsamak <vikramsamak02@gmail.com>
2024-07-23 22:15:41 +0530

    Added graviation route.

--- main.py
+++ main.py
@@ -61,6 +61,9 @@ app.include_router(SubAutomatic_Router, prefix="/subautomatic")
 # NLM Routes
 app.include_router(NLMRouter, prefix="/nlm")
 
+#Gravitation Route
+app.include_router()
+
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
