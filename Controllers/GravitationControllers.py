33abf7d920394d0028cdcbcfacc7cb6cf7ecc0e4
vikramsamak <vikramsamak02@gmail.com>
2024-08-25 13:41:47 +0530

    Added Gravitation dictionary of definitions.

--- Controllers/GravitationControllers.py
+++ Controllers/GravitationControllers.py
@@ -1,31 +1,40 @@
 from Libs.PhsiPy.PhsipyEquations import Gravitation
 from Schemas.GravitationSchema import *
 from Schemas.GenericSchema import GenericResponse
+from Dictionaries import GravitationDictionary
 
 
 def get_G(req: G_Request) -> GenericResponse[float]:
     g = Gravitation.G(req.m1, req.m2, req.r)
     return GenericResponse(
-        Definition="", Given={"M1": req.m1, "M2": req.m2, "R": req.r}, Result=g
+        Definition=GravitationDictionary.GravitationDict["G"],
+        Given={"M1": req.m1, "M2": req.m2, "R": req.r},
+        Result=g,
     )
 
 
 def get_G_Potential(req: G_Potential_Request) -> GenericResponse[float]:
     g_potential = Gravitation.G_Potential(req.M, req.r)
     return GenericResponse(
-        Definition="", Given={"M": req.M, "r": req.r}, Result=g_potential
+        Definition=GravitationDictionary.GravitationDict["g_potential"],
+        Given={"M": req.M, "r": req.r},
+        Result=g_potential,
     )
 
 
 def get_g_in_depth(req: G_In_Depth_Request) -> GenericResponse[float]:
     g_in_depth = Gravitation.g_in_depth(req.depth)
-    return GenericResponse(Definition="", Given={"Depth": req.depth}, Result=g_in_depth)
+    return GenericResponse(
+        Definition=GravitationDictionary.GravitationDict["g_in_depth"],
+        Given={"Depth": req.depth},
+        Result=g_in_depth,
+    )
 
 
 def get_axial_velocity(req: Axial_Velocity_Request) -> GenericResponse[float]:
     axial_velocity = Gravitation.axial_velocity(req.area_swept, req.time)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["axial_velocity"],
         Given={"Area Swept": req.area_swept, "Time": req.time},
         Result=axial_velocity,
     )
@@ -36,7 +45,7 @@ def get_gravitational_force(req: Gravitational_Force_Request) -> GenericResponse
         req.mass1, req.mass2, req.distance
     )
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["gravitational_force"],
         Given={"Mass 1": req.mass1, "Mass 2": req.mass2, "Distance": req.distance},
         Result=gravitational_force,
     )
@@ -49,7 +58,9 @@ def get_gravitational_potential_energy(
         req.mass1, req.mass2, req.distance
     )
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict[
+            "gravitational_potential_energy"
+        ],
         Given={"Mass 1": req.mass1, "Mass 2": req.mass2, "Distance": req.distance},
         Result=gravitational_potential_energy,
     )
@@ -62,7 +73,9 @@ def get_gravitational_field_strength(
         req.mass, req.distance
     )
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict[
+            "gravitational_field_strength"
+        ],
         Given={"Mass": req.mass, "Distance": req.distance},
         Result=gravitational_field_strength,
     )
@@ -71,7 +84,7 @@ def get_gravitational_field_strength(
 def get_escape_velocity(req: Escape_Velocity_Request) -> GenericResponse[float]:
     escape_velocity = Gravitation.escape_velocity(req.mass, req.radius)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["escape_velocity"],
         Given={"Mass": req.mass, "Radius": req.radius},
         Result=escape_velocity,
     )
@@ -80,7 +93,7 @@ def get_escape_velocity(req: Escape_Velocity_Request) -> GenericResponse[float]:
 def get_orbital_velocity(req: Orbital_Velocity_Request) -> GenericResponse[float]:
     orbital_velocity = Gravitation.orbital_velocity(req.mass, req.radius)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["orbital_velocity"],
         Given={"Mass": req.mass, "Radius": req.radius},
         Result=orbital_velocity,
     )
@@ -89,7 +102,7 @@ def get_orbital_velocity(req: Orbital_Velocity_Request) -> GenericResponse[float
 def get_period_of_orbit(req: Period_Of_Orbit_Request) -> GenericResponse[float]:
     period_of_orbit = Gravitation.period_of_orbit(req.mass, req.radius)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["period_of_orbit"],
         Given={"Mass": req.mass, "Radius": req.radius},
         Result=period_of_orbit,
     )
@@ -102,7 +115,7 @@ def get_gravitational_potential(
         req.mass, req.distance
     )
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["gravitational_potential"],
         Given={"Mass": req.mass, "Distance": req.distance},
         Result=gravitational_potential,
     )
@@ -111,7 +124,7 @@ def get_gravitational_potential(
 def get_weight(req: Weight_Request) -> GenericResponse[float]:
     weight = Gravitation.weight(req.mass, req.acceleration_due_to_gravity)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["weight"],
         Given={
             "Mass": req.mass,
             "Acceleration due to gravity": req.acceleration_due_to_gravity,
@@ -127,7 +140,7 @@ def get_gravitational_acceleration(
         req.mass, req.distance
     )
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["gravitational_acceleration"],
         Given={"Mass": req.mass, "Distance": req.distance},
         Result=gravitational_acceleration,
     )
@@ -136,7 +149,7 @@ def get_gravitational_acceleration(
 def get_keplers_third_law(req: Keplers_Third_Law_Request) -> GenericResponse[float]:
     keplers_third_law = Gravitation.keplers_third_law(req.period, req.semi_major_axis)
     return GenericResponse(
-        Definition="",
+        Definition=GravitationDictionary.GravitationDict["keplers_third_law"],
         Given={"Period": req.period, "Semi Major Axis": req.semi_major_axis},
         Result=keplers_third_law,
     )
