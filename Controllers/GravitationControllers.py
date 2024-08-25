fad8e6cd9fdf55e3c80e771723893e0ecc456291
vikramsamak <vikramsamak02@gmail.com>
2024-08-25 10:57:12 +0530

    Gravitation controllers added.

--- Controllers/GravitationControllers.py
+++ Controllers/GravitationControllers.py
@@ -4,64 +4,139 @@ from Schemas.GenericSchema import GenericResponse
 
 
 def get_G(req: G_Request) -> GenericResponse[float]:
-    return ""
+    g = Gravitation.G(req.m1, req.m2, req.r)
+    return GenericResponse(
+        Definition="", Given={"M1": req.m1, "M2": req.m2, "R": req.r}, Result=g
+    )
 
 
 def get_G_Potential(req: G_Potential_Request) -> GenericResponse[float]:
-    return ""
+    g_potential = Gravitation.G_Potential(req.M, req.r)
+    return GenericResponse(
+        Definition="", Given={"M": req.M, "r": req.r}, Result=g_potential
+    )
 
 
 def get_g_in_depth(req: G_In_Depth_Request) -> GenericResponse[float]:
-    return ""
+    g_in_depth = Gravitation.g_in_depth(req.depth)
+    return GenericResponse(Definition="", Given={"Depth": req.depth}, Result=g_in_depth)
 
 
 def get_axial_velocity(req: Axial_Velocity_Request) -> GenericResponse[float]:
-    return ""
+    axial_velocity = Gravitation.axial_velocity(req.area_swept, req.time)
+    return GenericResponse(
+        Definition="",
+        Given={"Area Swept": req.area_swept, "Time": req.time},
+        Result=axial_velocity,
+    )
 
 
 def get_gravitational_force(req: Gravitational_Force_Request) -> GenericResponse[float]:
-    return ""
+    gravitational_force = Gravitation.gravitational_force(
+        req.mass1, req.mass2, req.distance
+    )
+    return GenericResponse(
+        Definition="",
+        Given={"Mass 1": req.mass1, "Mass 2": req.mass2, "Distance": req.distance},
+        Result=gravitational_force,
+    )
 
 
 def get_gravitational_potential_energy(
     req: Gravitational_Potential_Energy_Request,
 ) -> GenericResponse[float]:
-    return ""
+    gravitational_potential_energy = Gravitation.gravitational_potential_energy(
+        req.mass1, req.mass2, req.distance
+    )
+    return GenericResponse(
+        Definition="",
+        Given={"Mass 1": req.mass1, "Mass 2": req.mass2, "Distance": req.distance},
+        Result=gravitational_potential_energy,
+    )
 
 
 def get_gravitational_field_strength(
     req: Gravitational_Field_Strength_Request,
 ) -> GenericResponse[float]:
-    return ""
+    gravitational_field_strength = Gravitation.gravitational_field_strength(
+        req.mass, req.distance
+    )
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Distance": req.distance},
+        Result=gravitational_field_strength,
+    )
 
 
 def get_escape_velocity(req: Escape_Velocity_Request) -> GenericResponse[float]:
-    return ""
+    escape_velocity = Gravitation.escape_velocity(req.mass, req.radius)
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Radius": req.radius},
+        Result=escape_velocity,
+    )
 
 
 def get_orbital_velocity(req: Orbital_Velocity_Request) -> GenericResponse[float]:
-    return ""
+    orbital_velocity = Gravitation.orbital_velocity(req.mass, req.radius)
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Radius": req.radius},
+        Result=orbital_velocity,
+    )
 
 
 def get_period_of_orbit(req: Period_Of_Orbit_Request) -> GenericResponse[float]:
-    return ""
+    period_of_orbit = Gravitation.period_of_orbit(req.mass, req.radius)
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Radius": req.radius},
+        Result=period_of_orbit,
+    )
 
 
 def get_gravitational_potential(
     req: Gravitational_Potential_Request,
 ) -> GenericResponse[float]:
-    return ""
+    gravitational_potential = Gravitation.gravitational_potential(
+        req.mass, req.distance
+    )
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Distance": req.distance},
+        Result=gravitational_potential,
+    )
 
 
 def get_weight(req: Weight_Request) -> GenericResponse[float]:
-    return ""
+    weight = Gravitation.weight(req.mass, req.acceleration_due_to_gravity)
+    return GenericResponse(
+        Definition="",
+        Given={
+            "Mass": req.mass,
+            "Acceleration due to gravity": req.acceleration_due_to_gravity,
+        },
+        Result=weight,
+    )
 
 
 def get_gravitational_acceleration(
     req: Gravitational_Acceleration_Request,
 ) -> GenericResponse[float]:
-    return ""
+    gravitational_acceleration = Gravitation.gravitational_acceleration(
+        req.mass, req.distance
+    )
+    return GenericResponse(
+        Definition="",
+        Given={"Mass": req.mass, "Distance": req.distance},
+        Result=gravitational_acceleration,
+    )
 
 
 def get_keplers_third_law(req: Keplers_Third_Law_Request) -> GenericResponse[float]:
-    return ""
+    keplers_third_law = Gravitation.keplers_third_law(req.period, req.semi_major_axis)
+    return GenericResponse(
+        Definition="",
+        Given={"Period": req.period, "Semi Major Axis": req.semi_major_axis},
+        Result=keplers_third_law,
+    )
